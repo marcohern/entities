17aa20ab0f3c73aa6ad112a9c6d3782dcf4b4e73
Marco Hernandez <marcohern@gmail.com>
2022-02-13 21:06:58 -0500

    added esp

---
+++ src/EntitiesServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Marcohern\Registrar;
+
+use Illuminate\Support\ServiceProvider;
+
+class EntitiesServiceProvider extends ServiceProvider {
+  public function boot() {
+    
+  }
+
+  public function register() {
+    
+  }
+}
